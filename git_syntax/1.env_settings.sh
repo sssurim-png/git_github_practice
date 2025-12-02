ecccc94f203d8a38d298d7c1007dd05dc92b1a16
sssurim@gmail.com <sssurim@gmail.com>
2025-12-02 15:03:33 +0900

    fsd

--- git_syntax/1.env_settings.sh
+++ git_syntax/1.env_settings.sh
@@ -30,12 +30,12 @@ git config --list
 #git프로젝트 생성방법 2가지
 1. 회사에 처음 입사했을 때 =>clone만
 2. 내 개인프로젝트를 로컬에서 진행하다가 업로드 하는 경우
- 2-1)github repo생성 -> clone만
- 2-2)github repo생성 + 내 로컬에 프로젝트를 github upload
- #1.원격에서 repo생성 후 clone
+ #2-1)github repo생성 -> clone만
+ #2-2)github repo생성 + 내 로컬에 프로젝트를 github upload
+ #1.원격에서 repo생성 후 clone <<추천
  #2.로컬에 이미 만들어진 프로젝트 github에 업로드
  #2-1 git init
-2-2)원격지 주소를 추가
+#2-2)원격지 주소를 추가
  git remote add origin 원격지 주소
 
 
@@ -43,4 +43,16 @@ git config --list
  원격정보
  히스토리(commit이력)
  =>git삭제 시 -> 다시만들면 git재생성(깡통)
+
+ .git폴더가 있는 곳에서 init이런걸 써야된다*
+-원격지 주소 -clone 받으면 남의 주소를 내 주소로 변경하거나(히스토리도 남기고) 기존에 있는거 삭제
+-히스토리  
+
+ #다른 repo에서 clone을 받아 나의repo로 옮기기
+ #1/커밍이력 그대로 가져가기
+
+ git remote  set-url origin 레포주소 #원격지 주소 변경
+ #커밍 이력 없이 가져가기 
+
+ git checkout -b main #main브랜치 생성 및 변경
  
