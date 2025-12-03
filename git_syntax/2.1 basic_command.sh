034ef54911d10de2d87ca4ac641236bf98301657
sssurim@gmail.com <sssurim@gmail.com>
2025-12-03 16:24:39 +0900

    asdf

--- git_syntax/2.1 basic_command.sh
+++ git_syntax/2.1 basic_command.sh
@@ -64,3 +64,33 @@ git reset head
 
 #이미 push commit사항을  되돌리는 새로운 commit 생성 (지워지는 것이 아님)
 git revert 커밋ID
+
+#작업 중인 사항을 임시 저장
+git stash
+#임시 저장한 작업사항을 다시 적용
+git stash pop
+#저장한 작업목록 조회
+git stash list
+#저장한 목록 전체 삭제
+git stash clear
+
+#가장 최신의 commitID에 tag가 명시
+git tag 태그버전명(v1.10.4)
+#원격 repo에 해당 tag의 release 생성
+git push origin 태그버전명
+#태그 목록 조회
+git tag
+
+#브랜치 생성
+git branch 브랜치명
+#현재 checkout 되어있는 branch를 기준으로 새로운 branch 생성
+git branch feat/member_list
+#브랜치 목록 조회
+git branch
+#특정 브랜치로 전환
+git checkout 브랜치명
+#브랜치 생성과 전환을 동시에
+git checkout -b 브랜치명
+#로컬 브랜치 삭제(원격은 별도로 삭제)
+git branch -D 브랜치명
+ 
